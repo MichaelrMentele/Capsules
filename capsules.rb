1614b3a0f0bd10c6a7ac530b87d66d7e020b3a8e
Michael Mentele <Michaelrmentele@gmail.com>
2016-05-04 21:16:37 -0700

    Implemented send messaging using the twilio api

--- capsules.rb
+++ capsules.rb
@@ -1,16 +1,37 @@
+require "yaml"
+require "twilio-ruby"
 require "sinatra"
 require "sinatra/reloader" if development?
 require "tilt/erubis"
 
 before do
+  @account_info = YAML.load_file("data/twilio_auth.yaml")
+  @twilio_sid = @account_info[:account_sid]
+  @token = @account_info[:auth_token]
 end
 
 get "/" do 
   @message = "I love you..."
+  send_message(@message)
 end
 
 helpers do 
   def random_message
 
+  end
+
+  def send_message(message)
+    client = Twilio::REST::Client.new(
+      @twilio_sid,
+      @token
+    )
+
+    client.messages.create(
+      to: "12066184282",
+      from: "14255288374",
+      body: message
+    )
+
+    puts "Message sent"
   end
 end
