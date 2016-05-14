d2352fbae6acf158a12925d49b89d0121f1b9909
Michael Mentele <Michaelrmentele@gmail.com>
2016-05-14 09:47:52 -0700

    Added flash messages to layout, added display of message collection, added display of sent messages  and deleted design docs because they are moved to their own location on freedcamp

--- capsules.rb
+++ capsules.rb
@@ -12,47 +12,83 @@ end
 
 before do
   session[:capsules] ||= []
+  session[:sent] ||= []
 
+  # Load Twilio account auth info
   account_info = YAML.load_file("data/twilio_auth.yaml")
   @twilio_sid = account_info[:account_sid]
   @token = account_info[:auth_token]
 end
 
+###########
+# Methods #
+###########
+
+# !!!
+def random_capsule
+  # error assertion if no message in session
+  session[:capsules].sample
+end
+
+# Send text via Twilio API
+def send_text(message)
+  client = Twilio::REST::Client.new(
+    @twilio_sid,
+    @token
+  )
+
+  client.messages.create(
+    to: "12066184282",
+    from: "14255288374",
+    body: message
+  )
+end
+
+def message_sent!(capsule)
+  session[:sent] << session[:capsules].delete(capsule)
+end
+
+########
+# GETs #
+########
+
+# Redirect to home page
 get "/" do 
   redirect "/capsules"
 end
 
+# Render messaging page
 get "/capsules" do 
+  @capsules = session[:capsules]
+  @sent_messages = session[:sent]
   erb :capsules, layout: :layout
 end
 
-get "/send" do 
-  #require 'pry'; binding.pry;
-  send_text(random_message)
-  redirect "/capsules"
-end
+#########
+# POSTs #
+#########
 
+# Add a message to the user's collection
 post "/capsules" do 
   session[:capsules] << params[:capsule_message]
   redirect "/capsules"
 end
 
+# !!!
+# Send a text message
+post "/send" do 
+  # Update to remove from collection and into the 'sent' list
+  capsule = random_capsule
+  send_text(capsule)
+
+  message_sent!(capsule)
+
+  redirect "/capsules"
+end
+
+###########
+# Helpers #
+###########
 helpers do 
-  def random_message
-    # error assertion if no message provided
-    session[:capsules].sample
-  end
-
-  def send_text(message)
-    client = Twilio::REST::Client.new(
-      @twilio_sid,
-      @token
-    )
-
-    client.messages.create(
-      to: "12066184282",
-      from: "14255288374",
-      body: message
-    )
-  end
+  
 end
